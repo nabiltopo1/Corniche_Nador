e3d54d6f59327cd608d22f1f14541555456faba2
nabiltopo1 <nabil.boutrik1@gmail.com>
2024-03-15 23:25:20 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,302 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama1",
+      "name": "Panorama1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20660513253978685,
+          "pitch": 0.0679643298214323,
+          "rotation": 2.356194490192345,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": 1.5872314936549703,
+          "pitch": 0.18278450437864358,
+          "rotation": 3.141592653589793,
+          "target": "2-panorama3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05580178101828359,
+          "pitch": 0.3896636174591528,
+          "title": "Site du Projet",
+          "text": "Les Tarrasses de la Lagune"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama2",
+      "name": "Panorama2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -1.0468379583417047,
+        "pitch": 0.2988394532573704,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22074277233867434,
+          "pitch": -0.00822017501129757,
+          "rotation": 2.356194490192345,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": 0.37194076199793,
+          "pitch": -0.00670641795326965,
+          "rotation": 3.141592653589793,
+          "target": "0-panorama1"
+        },
+        {
+          "yaw": 0.19538866868407823,
+          "pitch": 0.16401496001270743,
+          "rotation": 0,
+          "target": "3-panorama4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15901400568093038,
+          "pitch": 0.19746793578096344,
+          "title": "Site du Projet",
+          "text": "Les Terrasses de la Lagune"
+        }
+      ]
+    },
+    {
+      "id": "2-panorama3",
+      "name": "Panorama3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -1.0304570090209868,
+        "pitch": 0.050885220157113054,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23372255959448296,
+          "pitch": -0.012779414981569204,
+          "rotation": 3.141592653589793,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": -0.338145411037976,
+          "pitch": -0.032798802929740134,
+          "rotation": 3.141592653589793,
+          "target": "0-panorama1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama4",
+      "name": "Panorama4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.20598025445910295,
+        "pitch": 0.1910794537431002,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5895886289130736,
+          "pitch": -0.33017152447665765,
+          "rotation": 2.356194490192345,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": 2.238777378892097,
+          "pitch": -0.16756466277358584,
+          "rotation": 2.356194490192345,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": -2.892349542955195,
+          "pitch": -0.26443425202237236,
+          "rotation": 3.141592653589793,
+          "target": "0-panorama1"
+        },
+        {
+          "yaw": -1.9067995170429057,
+          "pitch": -0.2955931565185814,
+          "rotation": 0,
+          "target": "4-panorama5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama5",
+      "name": "Panorama5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.10657396188892143,
+        "pitch": 0.16496434485391198,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7471963805025155,
+          "pitch": 0.012783404216895278,
+          "rotation": 3.9269908169872414,
+          "target": "0-panorama1"
+        },
+        {
+          "yaw": 1.008059998339622,
+          "pitch": 0.05766387030099196,
+          "rotation": 3.141592653589793,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": 0.262635276616793,
+          "pitch": 0.3509214879474527,
+          "rotation": 0,
+          "target": "3-panorama4"
+        },
+        {
+          "yaw": -0.6973259856780665,
+          "pitch": 0.03923327918219499,
+          "rotation": 2.356194490192345,
+          "target": "1-panorama2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.07944897331348955,
+          "pitch": 0.2882012923949766,
+          "title": "Site du Projet",
+          "text": "Les Terrasses de la Lagune"
+        }
+      ]
+    }
+  ],
+  "name": "Les Terrasses de la lagune",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
